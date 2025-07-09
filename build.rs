925c6c91836db81d7eddfa0ffee69d4997b57415
Leon1777 <168653957+Leon1777@users.noreply.github.com>
2025-07-09 21:05:09 +0200

    remove asm

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto");
-    println!("cargo:rerun-if-changed=src/asm");
     tonic_build::configure()
         .build_server(false)
         .compile_protos(&["proto/rpc.proto", "proto/messages.proto"], &["proto"])?;
